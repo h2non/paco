868d9fa598c7d5480355cd7428bd9304114e3c28
Tomas Aparicio <tomas@aparicio.me>
2016-12-09 01:23:38 +0000

    fix(docs): comment out latex sphinx settings

--- docs/conf.py
+++ docs/conf.py
@@ -265,31 +265,31 @@ htmlhelp_basename = 'doc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
-latex_elements = {
-     # The paper size ('letterpaper' or 'a4paper').
-     #
-     # 'papersize': 'letterpaper',
-
-     # The font size ('10pt', '11pt' or '12pt').
-     #
-     # 'pointsize': '10pt',
-
-     # Additional stuff for the LaTeX preamble.
-     #
-     # 'preamble': '',
-
-     # Latex figure (float) alignment
-     #
-     # 'figure_align': 'htbp',
-}
+# latex_elements = {
+#      # The paper size ('letterpaper' or 'a4paper').
+#      #
+#      # 'papersize': 'letterpaper',
+#
+#      # The font size ('10pt', '11pt' or '12pt').
+#      #
+#      # 'pointsize': '10pt',
+#
+#      # Additional stuff for the LaTeX preamble.
+#      #
+#      # 'preamble': '',
+#
+#      # Latex figure (float) alignment
+#      #
+#      # 'figure_align': 'htbp',
+# }
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (master_doc, '.tex', ' Documentation',
-     'Tomas Aparicio', 'manual'),
-]
+# latex_documents = [
+#     (master_doc, '.tex', ' Documentation',
+#      'Tomas Aparicio', 'manual'),
+# ]
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
